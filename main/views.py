871662e75005c6312679661537ae4975cf51fe74
PedroCtba <miyasakigazola@gmail.com>
2022-08-07 13:02:23 -0300

    build: set sqllite database | add dynamic views to main app (using models at sqlite database)

--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import ToDoList, Item
 
-def index(response):
-    return HttpResponse("<h1> Index </h1>")
+def index(response, name):
+    ls = ToDoList.objects.get(name=name)
+    item = ls.item_set.get(id=1)
+
+    return HttpResponse("""
+    <h1> %s </h1>
+    <br></br>
+    <p>%s</p>
+    """     
+        % (ls.name, str(item.text)))
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'main.apps.MainConfig',
+    'main.apps.MainConfig', # Its necessary to register your app here if you want it to have a database
 ]
 
 MIDDLEWARE = [
