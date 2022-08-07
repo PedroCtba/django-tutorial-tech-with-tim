213e49ffb37310a0fab83be0b32506c08fdd8899
PedroCtba <miyasakigazola@gmail.com>
2022-08-07 16:02:26 -0300

    build: create form creation basic page

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,11 @@ from . import views
 urlpatterns = [
     # Path for home page
     path("", views.home),
+    
+    # Paths for todo list's
     path("<int:id>", views.index, name="index"),
+
+    # Path for creating a todo list
+    path("create/", views.create, name="create"),
+
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 from .models import ToDoList
+from .forms import createNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
@@ -7,3 +9,17 @@ def index(response, id):
 
 def home(response):
     return render(response, "main/home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = createNewList(response.POST)
+
+        if form.is_valid():
+            n = form.cleaned_data["name"]
+            t = ToDoList(name=n)
+            t.save()
+
+        return HttpResponseRedirect("/%i" %t.id)
+    else:
+        form = createNewList()
+    return render(response, "main/create.html", {"form": form})
