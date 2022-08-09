13ba79b73b7f5306303ee8217b20f398df4602d8
PedroCtba <miyasakigazola@gmail.com>
2022-08-09 12:42:28 -0300

    build: add forms and side bar to main app

--- main/views.py
+++ main/views.py
@@ -5,6 +5,27 @@ from .forms import createNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                
+                item.save()
+
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 1:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid")
+
+
     return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
@@ -20,6 +41,7 @@ def create(response):
             t.save()
 
         return HttpResponseRedirect("/%i" %t.id)
+        
     else:
         form = createNewList()
     return render(response, "main/create.html", {"form": form})
